1a74d2c2a980a3ade03eda52dae477de71b214ac
linlinliu <linlin.liu@ihandysoft.com>
2016-06-06 23:35:52 +0800

    update login component

--- modules/components/Login.js
+++ modules/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import AutoComplete from 'material-ui/AutoComplete';
+import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton'
 import ImageNature from 'material-ui/svg-icons/image/nature';
@@ -8,20 +9,22 @@ import ImageNature from 'material-ui/svg-icons/image/nature';
 
 export default React.createClass({
     getInitialState: function () {
-        return {dataSource: []};
+        return {username: '', names: [], password: ''};
     },
 
-    handleUpdateInput: function (value) {
+    handleUpdateUsernameInput: function (value) {
         this.setState({
-            dataSource: [
-                value,
-                value + value,
-                value + value + value,
-            ],
+            names: ['jack', 'lily', 'bill'],
+            username: value
         });
     },
-    onMouseDown: function (event) {
-        console.log('clicked login button');
+    handleChange: function (event) {
+        this.setState({
+            password: event.target.value
+        });
+    },
+    handleSubmit: function () {
+        console.log(this.state);
     },
     render: function () {
         return (
@@ -30,27 +33,30 @@ export default React.createClass({
                     title="Plant"
                     iconElementLeft={<IconButton><ImageNature /></IconButton>}
                 />
-                <AutoComplete
-                    hintText="Type Username"
-                    dataSource={this.state.dataSource}
-                    onUpdateInput={this.handleUpdateInput}
-                    floatingLabelText="Username"
-                    fullWidth={true}
-                />
-                <AutoComplete
-                    hintText="Type Password"
-                    dataSource={this.state.dataSource}
-                    onUpdateInput={this.handleUpdateInput}
-                    floatingLabelText="Password"
-                    fullWidth={true}
-                />
-                <FlatButton
-                    label="Login"
-                    primary={true}
-                    linkButton={true}
-                    href="#/"
-                    onMouseDown={this.onMouseDown}
-                />
+                <form>
+                    <AutoComplete
+                        hintText="Type Username"
+                        dataSource={this.state.names}
+                        onUpdateInput={this.handleUpdateUsernameInput}
+                        floatingLabelText="Username"
+                        fullWidth={true}
+                    />
+                    <TextField
+                        hintText="Type Password"
+                        floatingLabelText="Password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        type="password"
+                        fullWidth={true}
+                    />
+                    <FlatButton
+                        label="Login"
+                        primary={true}
+                        // linkButton={true}
+                        // href="#/"
+                        onTouchTap={this.handleSubmit}
+                    />
+                </form>
             </div>
         );
     },
